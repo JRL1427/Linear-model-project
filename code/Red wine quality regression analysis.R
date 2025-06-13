befe3e709477f4794581cea7b565a7ab09193ebb
JRL1427 <143640818+JRL1427@users.noreply.github.com>
2025-06-14 01:13:58 +0800

    Linear model not work

--- code/Red wine quality regression analysis.R
+++ code/Red wine quality regression analysis.R
@@ -1,6 +1,24 @@
-winedata <- read.table('winequality-red.csv', sep = ';', header = TRUE)
+winedata <- read.table('D:\\Backup\\Documents\\GitHub\\portfolio\\data\\winequality-red.csv', sep = ';', header = TRUE)
 fit <- lm(quality ~ ., data = winedata)
+summary(fit)
+plot(fitted.values(fit), jitter(rstandard(fit)))
+plot(rstandard(fit))
+length(rstandard(fit)[rstandard(fit) > 4]) + length(rstandard(fit)[rstandard(fit) < -4])
+qqnorm(rstandard(fit))
+abline(a=0,b=1)
+
+
+library(nortest)
+ad.test(rstandard(fit))
+
 library(MASS)
-fitsl <- step(fit, direction = 'backward')
+fitsl <- step(fit, direction = 'both')
 
 library(car)
+vif(fitsl)
+anova(fitsl,fit)
+
+summary(fitsl)
+ad.test(rstandard(fitsl))
+
+plot(fit)
