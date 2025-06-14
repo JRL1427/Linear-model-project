68e27a558101196d32d8f1d6c038dc5a58e0efbf
JRL1427 <143640818+JRL1427@users.noreply.github.com>
2025-06-14 17:20:35 +0100

    ordinal rergression

--- code/Red wine quality regression analysis.R
+++ code/Red wine quality regression analysis.R
@@ -1,4 +1,6 @@
 winedata <- read.table('D:\\Backup\\Documents\\GitHub\\portfolio\\data\\winequality-red.csv', sep = ';', header = TRUE)
+winedata <- read.table('/Users/max/Documents/Github/Red-wine-quality-regression-analysis/data/winequality-red.csv',sep =';',header = TRUE)
+
 fit <- lm(quality ~ ., data = winedata)
 summary(fit)
 plot(fitted.values(fit), jitter(rstandard(fit)))
@@ -15,10 +17,15 @@ library(MASS)
 fitsl <- step(fit, direction = 'both')
 
 library(car)
+vif(fit)
 vif(fitsl)
 anova(fitsl,fit)
 
 summary(fitsl)
 ad.test(rstandard(fitsl))
-
 plot(fit)
+
+install.packages('ordinal')
+library(ordinal)
+fito <- clm(as.factor(quality)~ ., data = winedata, link = 'logit')
+vif(fito)
